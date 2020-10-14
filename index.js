b55251776db55f6c58fee5d437aae0cf4bdf78b0
Brian Ballsun-Stanton <brian@fedarch.org>
2020-10-14 17:30:27 +1100

    uibuild

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3001
 
 app.use(cors())
 // invoking this middlware before making morgan tokens shouldn't matter, but...
+app.use(express.static('build'))
 
 
 app.use(express.json()) //Well rememering this took me 2 bloody hours.
