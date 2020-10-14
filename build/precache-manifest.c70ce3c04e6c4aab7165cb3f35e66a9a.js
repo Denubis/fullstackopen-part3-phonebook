a0770eeb6f41749bb407d57d3b5c0b6bdee370bd
Brian Ballsun-Stanton <brian@fedarch.org>
2020-10-14 17:30:27 +1100

    uibuild

---
+++ build/precache-manifest.c70ce3c04e6c4aab7165cb3f35e66a9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ae725307119724ba8ede1bd5d48af6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d466e79cfa52ee0bb24",
+    "url": "/static/css/main.134e9fdd.chunk.css"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d466e79cfa52ee0bb24",
+    "url": "/static/js/main.c0a18991.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
